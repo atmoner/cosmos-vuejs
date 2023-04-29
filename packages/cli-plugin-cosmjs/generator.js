f73027e03515c8b40d631d932c86128753c23631
atmoner <contact.atmoner@gmail.com>
2023-04-29 22:08:50 +0200

    Update cosmVue

--- packages/cli-plugin-cosmjs/generator.js
+++ packages/cli-plugin-cosmjs/generator.js
@@ -1,27 +1,26 @@
 module.exports = api => {
-  console.log(api.options)
-  if (api.options.cosmjsSelectedFeatures === 'stargate') {
+  if (api.options.find(element => element === 'stargate')) {
     api.extendPackage({
       dependencies: {
         '@cosmjs/stargate': '^0.30.1',
       },
     });
   }  
-  if (api.options.cosmjsSelectedFeatures === 'cosmwasmstargate') {
+  if (api.options.find(element => element === 'cosmwasmstargate')) {
     api.extendPackage({
       dependencies: {
         '@cosmjs/cosmwasm-stargate': '^0.30.1',
       },
     });
   } 
-  if (api.options.cosmjsSelectedFeatures === 'protosigning') {
+  if (api.options.find(element => element === 'protosigning')) {
     api.extendPackage({
       dependencies: {
         '@cosmjs/proto-signing': '^0.30.1',
       },
     });
   } 
-  if (api.options.cosmjsSelectedFeatures === 'ledgeramino') {
+  if (api.options.find(element => element === 'ledgeramino')) {
     api.extendPackage({
       dependencies: {
         '@cosmjs/ledger-amino': '^0.30.1',
