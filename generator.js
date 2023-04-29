70dc458d5a4326644d9477c0d1463ad5b90e92a5
atmoner <contact.atmoner@gmail.com>
2023-04-29 21:00:12 +0200

    Add vue api

---
+++ generator.js
@@ -0,0 +1,21 @@
+module.exports = api => {
+  api.extendPackage({
+    description: api.options.description
+  });
+
+  if (api.options.axios) {
+    api.extendPackage({
+      dependencies: {
+        axios: '^0.20.0',
+      },
+    });
+  }
+
+  if (api.options.cosmjs) {
+    api.extendPackage({
+      dependencies: {
+        '@cosmjs/stargate': '^0.30.1',
+      },
+    });
+  }  
+};
---
+++ prompts.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    name: 'axios',
+    type: 'confirm',
+    message: 'Install axios?',
+  },
+  {
+    name: 'cosmjs',
+    type: 'confirm',
+    message: 'Install cosmjs?',
+  },  
+];
