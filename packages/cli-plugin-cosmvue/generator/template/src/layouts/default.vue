5165eee3df8ac633e7965ee355b8c538c22fcc40
atmoner <contact.atmoner@gmail.com>
2023-04-30 00:43:21 +0200

    Update cosmVue

---
+++ packages/cli-plugin-cosmvue/generator/template/src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app class="white" id="inspire">
+    <div id="app">
+      {{ message }}
+    </div>
+  </v-app>
+</template>    
+<script>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+</script>
+ 
