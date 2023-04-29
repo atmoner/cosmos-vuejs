381268bdef32584a85e08426093a656e79cc4dee
atmoner <contact.atmoner@gmail.com>
2023-04-29 21:28:46 +0200

    Update cosmVue

--- prompts.js
+++ prompts.js
@@ -3,10 +3,5 @@ module.exports = [
     name: 'axios',
     type: 'confirm',
     message: 'Install axios?',
-  },
-  {
-    name: 'cosmjs',
-    type: 'confirm',
-    message: 'Install cosmjs?',
-  },  
+  } 
 ];
