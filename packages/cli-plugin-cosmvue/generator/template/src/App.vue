500bc246f3b34af992b27bbb74cc06ba24a23b52
atmoner <contact.atmoner@gmail.com>
2023-05-18 21:48:46 +0200

    Update 1.0.6

--- packages/cli-plugin-cosmvue/generator/template/src/App.vue
+++ packages/cli-plugin-cosmvue/generator/template/src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <v-app class="white" id="inspire">
-    <div id="app">
-      {{ message }}
-    </div>
-  </v-app>
-</template>    
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
 <script>
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
-})
+}
 </script>
- 
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
